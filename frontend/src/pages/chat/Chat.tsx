17a1cf7d372aefc7bd00819e43187578003ddad9
bridodonnellesb <161495550+bridodonnellesb@users.noreply.github.com>
2024-04-24 17:05:26 +0100

    citation ui - removed citation text

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -791,16 +791,7 @@ const Chat = () => {
                                 <IconButton iconProps={{ iconName: 'Cancel' }} aria-label="Close citations panel" onClick={() => setIsCitationPanelOpen(false)} />
                             </Stack>
                             <h5 className={styles.citationPanelTitle} tabIndex={0} title={activeCitation.url && !activeCitation.url.includes("blob.core") ? activeCitation.url : activeCitation.title ?? ""} onClick={() => onViewSource(activeCitation)}>{activeCitation.title}</h5>
-                            <div tabIndex={0}>
-                                <ReactMarkdown
-                                    linkTarget="_blank"
-                                    className={styles.citationPanelContent}
-                                    children={DOMPurify.sanitize(activeCitation.content, {ALLOWED_TAGS: XSSAllowTags})}
-                                    remarkPlugins={[remarkGfm]}
-                                    rehypePlugins={[rehypeRaw]}
-                                />
-                            </div>
-                            <iframe src="https://datascienceteampocra7fd.blob.core.windows.net/cs-knowledgemanagement-sops/2023%20Smart%20Metering%20FAQ's.pdf#page=[2]" width="100%" min-height="50vh"></iframe>
+                            <iframe src="https://datascienceteampocra7fd.blob.core.windows.net/cs-knowledgemanagement-sops/2023%20Smart%20Metering%20FAQ's.pdf#page=[2]" width="100%" height="100%"></iframe>
                         </Stack.Item>
                     )}
                     {(appStateContext?.state.isChatHistoryOpen && appStateContext?.state.isCosmosDBAvailable?.status !== CosmosDBStatus.NotConfigured) && <ChatHistoryPanel />}
