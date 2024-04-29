595f97a86cdde77721cc2e732819a1497bffa3a4
bridodonnellesb <161495550+bridodonnellesb@users.noreply.github.com>
2024-04-29 14:44:44 +0100

    Basic PDF Citation View - answer

--- frontend/src/components/Answer/Answer.tsx
+++ frontend/src/components/Answer/Answer.tsx
@@ -71,21 +71,22 @@ export const Answer = ({
     const createCitationFilepath = (citation: Citation, index: number, truncate: boolean = false) => {
         let citationFilename = "";
 
+        // if (citation.filepath) {
+        //     const part_i = citation.part_index ?? (citation.chunk_id ? parseInt(citation.chunk_id) + 1 : '');
+        //     if (truncate && citation.filepath.length > filePathTruncationLimit) {
+        //         const citationLength = citation.filepath.length;
+        //         citationFilename = `${citation.filepath.substring(0, 20)}...${citation.filepath.substring(citationLength - 20)} - Part ${part_i}`;
+        //     }
+        //     else {
+        //         citationFilename = `${citation.filepath} - Part ${part_i}`;
+        //     }
+        // }
+        // else if (citation.filepath && citation.reindex_id) {
+        //     citationFilename = `${citation.filepath} - Part ${citation.reindex_id}`;
+        // }
+        // else {
         if (citation.filepath) {
-            const part_i = citation.part_index ?? (citation.chunk_id ? parseInt(citation.chunk_id) + 1 : '');
-            if (truncate && citation.filepath.length > filePathTruncationLimit) {
-                const citationLength = citation.filepath.length;
-                citationFilename = `${citation.filepath.substring(0, 20)}...${citation.filepath.substring(citationLength - 20)} - Part ${part_i}`;
-            }
-            else {
-                citationFilename = `${citation.filepath} - Part ${part_i}`;
-            }
-        }
-        else if (citation.filepath && citation.reindex_id) {
-            citationFilename = `${citation.filepath} - Part ${citation.reindex_id}`;
-        }
-        else {
-            citationFilename = `Citation ${index}`;
+            citationFilename = `${citation.title} - Part ${index}`;
         }
         return citationFilename;
     }
