9decf836a1f8a0650dcc7a25718946b0bea90ba0
bridodonnellesb <contractor.brid.odonnell@esb.ie>
2024-07-18 12:14:25 +0100

    Adding SAS for pdf preview

--- backend/history/cosmosdbservice.py
+++ backend/history/cosmosdbservice.py
@@ -2,6 +2,10 @@ import uuid
 from datetime import datetime
 from azure.cosmos.aio import CosmosClient
 from azure.cosmos import exceptions
+import json
+from backend.utils import (
+    generate_SAS
+)
   
 class CosmosConversationClient():
     
@@ -178,6 +182,11 @@ class CosmosConversationClient():
         query = f"SELECT * FROM c WHERE c.conversationId = @conversationId AND c.type='message' AND c.userId = @userId ORDER BY c.timestamp ASC"
         messages = []
         async for item in self.container_client.query_items(query=query, parameters=parameters):
+            if item["role"]=="tool":
+                content = json.loads(item["content"])
+                for i, chunk in enumerate(content["citations"]):
+                    content["citations"][i]["url"]=chunk["url"]+"?"+generate_SAS(chunk["url"])
+                item["content"] = json.dumps(content)
             messages.append(item)
 
         return messages
